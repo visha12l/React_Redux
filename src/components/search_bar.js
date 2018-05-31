f57a189679dfe46faf6601d88f7eac712c2ca144
visha12l <vsvshinde15@gmail.com>
2018-05-31 18:42:55 +0530

    code refactoring with eslint and video details functionality completed

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,38 +1,51 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-class Searchbar extends React.Component { //define new class searchbar and give all its ability to acces class mehtods and properties
+class Searchbar extends React.Component {
+  // define new class searchbar and give all its ability to acces class mehtods and properties
+  constructor() {
+    // it is a function called automatically whenever a new instance of a class is created
+    // i.e (whenever we call searchbar in parent component)
+    // it is used to initialize the state, variables
+    super(); //
+    this.state = { // this method is only used in constructor
+      inputValue: '',
+    }; // state is plain js object used to record and react to user events
+    // whenever state is changed it rerenders the component and all of its children component
+    // functional components do not have state
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
 
-    constructor() { //it is a function called automatically whenever a new instance of a class is created (whenever we call searchbar in parent component)
-        //it is used to initialize the state, variables
-        super(); //
-        this.state = {  //this mehtod is only used in constructor
-            inputValue: '',
-        }; //state is plain js object used to record and react to user events
-        //whenever state is changed it rerenders the component and all of its children component
-        //functional components do not have state
-        this.handleInputChange = this.handleInputChange.bind(this);
-    }
+  handleInputChange(event) {
+    this.setState({
+      inputValue: event.target.value,
+    });
+    this.props.onSearch(this.state.inputValue);
+    // make api call using search query
+  }
 
-    handleInputChange(event) {
-        this.setState({
-            inputValue: event.target.value
-        });
-        this.props.onSearch(this.state.inputValue);
-        //make api call using search query
-    }
-
-    render() { //this is compulsory method for class based components
-        //we can change state in render method also
-        //controlled input :: whose value is set by using state object so its value will change when state changes
-        return (
-            <div className="search-bar">
-                <input type='text'
-                    value={this.state.inputValue}
-                    placeholder='Search Video'
-                    onChange={this.handleInputChange}
-                />
-            </div>
-        );
-    }
+  render() { // this is compulsory method for class based components
+    // we can change state in render method also
+    // controlled input :: whose value is controlled by state object
+    return (
+      <div className="search-bar">
+        <input
+          type="text"
+          value={this.state.inputValue}
+          placeholder="Search Video"
+          onChange={this.handleInputChange}
+        />
+      </div>
+    );
+  }
 }
+
+Searchbar.defaultProps = {
+  onSearch: undefined,
+};
+
+Searchbar.propTypes = {
+  onSearch: PropTypes.func,
+};
+
 export default Searchbar;
--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,19 +1,47 @@
 import React from 'react';
-import underscore from 'underscore';
-import VideoListItem from './video_list_item';
+import _ from 'lodash';
+import PropTypes from 'prop-types';
 
-const VideoList = (props) => {
+class VideoList extends React.Component {
+  constructor() {
+    super();
+    this.selectVideo = this.selectVideo.bind(this);
+  }
+
+  selectVideo(video) {
+    this.props.selectVideo(video);
+  }
+
+  render() {
+    const { videos } = this.props;
     return (
-        <ul clasName="col-md-4 list-group">
-            {
-                underscore.map(props.videos, (video, key) => {
-                    return (
-                        <VideoListItem video={video} key={key} />
-                    )
-                })
-            }
-        </ul>
+      <ul className="col-md-4 list-group">
+        { _.map(videos, (video, key) => {
+          return (
+            <li key={key} onClick={this.selectVideo.bind(this, video)} className="list-group-item">
+              <div className="media-middle media-body pull-left">
+                <img className="media-object" src={video.snippet.thumbnails.default.url} alt="media object" />
+              </div>
+              <div className="media-body">
+                <h5 className="media-heading videoTitle"><strong>{video.snippet.title}</strong></h5>
+                <p className="channelTitle">{video.snippet.channelTitle}</p>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
     );
+  }
+}
+
+VideoList.defaultProps = {
+  videos: [],
+  selectVideo: undefined,
+};
+
+VideoList.propTypes = {
+  videos: PropTypes.arrayOf(PropTypes.object),
+  selectVideo: PropTypes.func,
 };
 
 export default VideoList;
